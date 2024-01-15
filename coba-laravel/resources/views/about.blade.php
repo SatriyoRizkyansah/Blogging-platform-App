e750d7579fdada578b9a73b5d9f2c23c07fa35e9
Satriyo Rizkyansah <satttrioo@gmail.com>
2024-01-15 16:50:34 +0700

    belahar laravel 8. 4/23.

--- coba-laravel/resources/views/about.blade.php
+++ coba-laravel/resources/views/about.blade.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+@extends('layouts.main')
 
-    <h1>Ini adalah halaman Blog!</h1>
+@section('container')
+    <h1>Ini adalah halaman About!</h1>
 
     <h2>Bio data diri</h2>
-    <h5>Nama : <?= $name; ?></h5>
-    <h5>Email : <?= $email; ?></h5>
-    <h5>Foto  : </h5><img src="<?= $img; ?>" alt="<?= $name; ?>"; width="200px">
-    
-    
-</body>
-</html>
+    <h5>Nama : {{ $name }} </h5>
+    <h5>Email : {{ $email }}</h5>
+    <h5>Foto  : </h5><img src="{{ $img }}" alt="{{ $name }}"; width="200px">
+@endsection
